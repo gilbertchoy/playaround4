d64edff89108de3af17fcd012bf86174dcb2ef01
bort ske <gilbertchoy@gmail.com>
2021-01-12 11:47:11 -0800

    old

---
+++ app/src/main/java/com/bort/playaround4/test.java
@@ -0,0 +1,23 @@
+package com.bort.playaround4;
+
+public class test {
+
+    String color;
+    String type;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
